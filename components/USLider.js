093cdfdb2840f06aa890676e0ec04f26dd93ed0f
Barbara <babsi.joebstl@gmail.com>
2018-09-08 15:16:18 +0200

    add first components, feat: switch between two comp

---
+++ components/USLider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default function USlider() {
+    return (
+        <View>
+            <Text>HELLO FROM SLIDER</Text>
+        </View>    
+    )
+}
